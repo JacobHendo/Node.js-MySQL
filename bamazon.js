e545f0339bf3fa739b34cdb78be5fad34e454057
Jacob Henderson <Hendersonjacob1492@gmail.com>
2018-08-21 10:06:15 -0400

    working order

--- bamazon.js
+++ bamazon.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-
+var selectedItem = "";
 var connection = mysql.createConnection({
   host: "localhost",
 
@@ -15,69 +15,80 @@ var connection = mysql.createConnection({
   //database name
   database: "bamazonDB"
 });
-
-connection.connect(function(err) {
+//test connection
+connection.connect(function (err) {
   if (err) throw err;
   startUp();
 });
 
 function startUp() {
-  connection.query("SELECT * FROM products", function(err, res) {
+  connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
+    console.table(res);
+  
     inquirer
-     .prompt([
-       {
-         name: "choice",
-         type: "rawlist",
-         choices: function() {
-           var choiceArray = [];
-           for (var i = 0; i < results.length; i++) {
-             choiceArray.push(results[i].item_name);
-           }
-           return choiceArray;
-         },
-         message: " What is the ID # of the product you would like to buy."
-       },
-       {
-         name: "bid",
-         type: "input",
-         message: "How many units would you like to buy?"
-       }
-     ])
-     // .then(function(answer) {
-     //   // get the information of the chosen item
-     //   var chosenItem;
-     //   for (var i = 0; i < results.length; i++) {
-     //     if (results[i].item_name === answer.choice) {
-     //       chosenItem = results[i];
-     //     }
-     //   }
-     //
-     //   // determine if bid was high enough
-     //   if (chosenItem.stock < parseInt(answer.bid)) {
-     //     // bid was high enough, so update db, let the user know, and start over
-     //     connection.query(
-     //       "UPDATE products SET ? WHERE ?",
-     //       [
-     //         {
-     //           stock: answer.bid
-     //         },
-     //         {
-     //           id: chosenItem.id
-     //         }
-     //       ],
-     //       function(error) {
-     //         if (error) throw err;
-     //         console.log("Bid placed successfully!");
-     //         start();
-     //       }
-     //     );
-     //   }
-     //   else {
-     //     // bid wasn't high enough, so apologize and start over
-     //     console.log("Your bid was too low. Try again...");
-     //     start();
-     //   }
-     // });
- });
-  }
+      .prompt([
+        {
+          name: "buying",
+          type: "input",
+          message: "what product id would you like to buy?",
+          validate: function (value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+        }
+        ,
+        {
+          name: "howMuch",
+          type: "input",
+          message: "How many would you like to buy? ",
+          validate: function (value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+        }
+
+      ])
+    
+     
+    .then(function(answer){
+
+      for (var i = 0; i < res.length; i++) {
+        if (res[i].id === parseInt(answer.buying)) {
+          selectedItem = res[i];
+        }
+      }
+      
+
+
+      if (parseInt(answer.howMuch) > selectedItem.stock) {
+        console.log("We don't have enough of that item available.\nPlease select a different item or change the quantity.");
+
+        startUp();
+      }
+       else {
+
+        console.log(("ORDER SUCCESSFUL!!! YOUR TOTAL IS $" + (selectedItem.price * parseInt(answer.howMuch)).toFixed(2)));
+        var newStock = parseInt(selectedItem.stock) - parseInt(answer.howMuch);
+
+        connection.query("UPDATE products SET ? WHERE ?",
+          [{
+            stock: newStock
+          },
+          {
+            id: answer.buying
+          }],
+          function (err) {
+            if (err) throw err;
+          })
+        startUp();
+      }
+    })
+  })
+}
+
+
